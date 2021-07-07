5261895c55555063bb4a3a52c7873d3348371aac
Samil Akpinar <samilakpinar6@gmail.com>
2021-07-07 09:47:01 +0300

    first commit

---
+++ src/app/cards/card-item/card-item.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Card } from 'src/app/models/card';
+import { CardModalComponent } from '../card-modal/card-modal.component';
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss']
+})
+export class CardItemComponent implements OnInit {
+ 
+  //Card tipinde tanımalama yapıldı
+  @Input() card!: Card;
+
+  constructor(
+    private dialog: MatDialog
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  openUpdateCardModal(card: Card): void {
+    this.dialog.open(CardModalComponent, {
+      width: '400px',
+      data: card
+    });
+  }
+
+}
---
+++ src/app/models/card.ts
@@ -0,0 +1,9 @@
+export interface Card {
+    id: number,
+    name?: string;
+    title: string;//zorunlu alandır
+    phone: string;//zorunlu alandır
+    email?: string;
+    address?: string;
+}
+
